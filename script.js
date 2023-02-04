576c17b3644c115ff533fb3e4f71ce1d4ec914be
Quinn Hertel <98055166+egg-bagel@users.noreply.github.com>
2023-02-04 10:53:22 -0500

    Basic calculator working correctly

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+let display = document.querySelector("#display");
+let numberButtons = document.querySelectorAll(".num");
+let operatorButtons = document.querySelectorAll(".operator");
+let equalsButton = document.querySelector("#equals");
+let clearButton = document.querySelector("#clear");
+
+let firstNumber;
+let currentOperator;
+let lastKeyOperator = false;
+let lastKeyEquals = false;
+let result;
+
+for (numberButton of numberButtons) {
+    numberButton.addEventListener("click", function(e) {
+        if ((display.innerText == "0") || (lastKeyOperator == true) || (lastKeyEquals == true)) {
+            display.innerText = e.target.innerText;
+            lastKeyOperator = false;
+            lastKeyEquals = false;
+        }
+        else {
+            display.innerText = display.innerText + e.target.innerText;
+        }
+    });
+}
+
+for (operatorButton of operatorButtons) {
+    operatorButton.addEventListener("click", function(e) {
+        if (firstNumber == undefined || currentOperator == undefined) {
+            firstNumber = display.innerText;
+            currentOperator = e.target.innerText;
+            lastKeyOperator = true;
+        }
+        else {
+            result = operate(parseFloat(firstNumber), parseFloat(display.innerText), currentOperator);
+            result = +result.toFixed(2);
+            display.innerText = result;
+            firstNumber = display.innerText;
+            currentOperator = e.target.innerText;
+            lastKeyOperator = true;
+        }
+    });
+}
+
+equalsButton.addEventListener("click", function() {
+    if (firstNumber && currentOperator && lastKeyEquals == false) {
+        result = operate(parseFloat(firstNumber), parseFloat(display.innerText), currentOperator);
+        result = +result.toFixed(2);
+        display.innerText = result;
+        currentOperator = undefined;
+        lastKeyEquals = true;
+    }
+    });
+
+clearButton.addEventListener("click", clear);
+
 function add(x, y) {
     return x + y;
 }
@@ -23,12 +78,18 @@ function operate(x, y, operator) {
     else if (operator == "-") {
         result = subtract(x, y);
     }
-    else if (operator == "*") {
+    else if (operator == "x") {
         result = multiply(x, y);
     }
-    else if (operator == "/") {
+    else if (operator == "÷") {
         result = divide(x, y);
     }
 
     return result;
+}
+
+function clear() {
+    display.innerText = 0;
+    firstNumber = undefined;
+    currentOperator = undefined;
 }
