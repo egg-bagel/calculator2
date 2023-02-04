74753f31a0813ca0255fb6f9be4b005e593c18e4
Quinn Hertel <98055166+egg-bagel@users.noreply.github.com>
2023-02-04 11:42:50 -0500

    Added decimal button and backspace button

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ let numberButtons = document.querySelectorAll(".num");
 let operatorButtons = document.querySelectorAll(".operator");
 let equalsButton = document.querySelector("#equals");
 let clearButton = document.querySelector("#clear");
+let decimalButton = document.querySelector("#decimal");
+let backspace = document.querySelector("#back");
 
 let firstNumber;
 let currentOperator;
 let lastKeyOperator = false;
 let lastKeyEquals = false;
+let decimalUsed = false;
 let result;
+
 
 for (numberButton of numberButtons) {
     numberButton.addEventListener("click", function(e) {
@@ -29,6 +33,7 @@ for (operatorButton of operatorButtons) {
             firstNumber = display.innerText;
             currentOperator = e.target.innerText;
             lastKeyOperator = true;
+            decimalUsed = false;
         }
         else {
             result = operate(parseFloat(firstNumber), parseFloat(display.innerText), currentOperator);
@@ -37,9 +42,25 @@ for (operatorButton of operatorButtons) {
             firstNumber = display.innerText;
             currentOperator = e.target.innerText;
             lastKeyOperator = true;
+            decimalUsed = false;
         }
     });
 }
+
+decimalButton.addEventListener("click", function(e) {
+    if (decimalUsed == false) {
+        if ((display.innerText == "0") || (lastKeyOperator == true) || (lastKeyEquals == true)) {
+            display.innerText = e.target.innerText;
+            lastKeyOperator = false;
+            lastKeyEquals = false;
+            decimalUsed = true;
+        }
+        else {
+            display.innerText = display.innerText + e.target.innerText;
+            decimalUsed = true;
+        }
+    }
+});
 
 equalsButton.addEventListener("click", function() {
     if (firstNumber && currentOperator && lastKeyEquals == false) {
@@ -48,8 +69,13 @@ equalsButton.addEventListener("click", function() {
         display.innerText = result;
         currentOperator = undefined;
         lastKeyEquals = true;
+        decimalUsed = false;
     }
-    });
+});
+
+backspace.addEventListener("click", function() {
+    display.innerText = display.innerText.substring(0, (display.innerText.length - 1));
+});
 
 clearButton.addEventListener("click", clear);
 
